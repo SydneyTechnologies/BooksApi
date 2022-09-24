a38f03e1e081433c3d9cf88b13e1ddd6cd8f1266
sydneyIdundun <sydney.idundun@gmail.com>
2022-09-25 02:11:53 +0400

    created a working login, token and logout view and added permissions (IsAuthenticated) globally

--- api/views.py
+++ api/views.py
@@ -1,8 +1,14 @@
-from django.shortcuts import render
 from rest_framework.generics import CreateAPIView, DestroyAPIView, ListAPIView, RetrieveUpdateAPIView
 from . models import Book
 from .serializers import BookSerializer
-
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from knox.models import AuthToken
+from django.contrib.auth import authenticate
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 class CreateBookApiView(CreateAPIView):
     # queryset is set to Book so drf knows what model to create with this endpoint
@@ -20,3 +26,25 @@ class ListBookApiView(ListAPIView):
 class DeleteBookApiView(DestroyAPIView):
     queryset =  Book.objects.all()
     serializer_class = BookSerializer
+
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = ["username", "password"]
+
+class LoginView(APIView):
+    # this view will allow us to get our token in one of the following ways
+    # through sending our login detail using a username and password (like signing in) 
+    serializer_class = UserSerializer
+    def post(self, request):
+        username = request.data["username"]
+        password = request.data["password"]
+        user = authenticate(username=username, password=password)
+        if user is None:
+            return Response({"Info":" Invalid credentials"})
+
+        return Response({
+            "Token": AuthToken.objects.create(user)[1]
+        })
--- booksApi/settings.py
+++ booksApi/settings.py
@@ -124,10 +124,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# REST_FRAMEWORK = {
-#     'DEFAULT_AUTHENTICATION_CLASSES': [
-#         'knox.auth.TokenAuthentication',
-#         'rest_framework.authentication.BasicAuthentication',
-#     ],
-# }
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'knox.auth.TokenAuthentication',
+    ],
 
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+
+}
